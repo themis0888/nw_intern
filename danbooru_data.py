311631ffd6828ec551061d783ef794719275caa6
Min Ki Jo <themis0888@gmail.com>
2018-07-19 21:16:03 +0900

    metadata added

--- danbooru_data.py
+++ danbooru_data.py
@@ -18,3 +18,16 @@ key_number_map = {}
 for i in range(num_used_key):
 	key_number_map[sorted_key_lst[i]] = i
 
+top_100_tags 	= {'1girl', 'bangs', 'greyscale', 'looking_at_viewer', 'monochrome', 'short_hair', 'solo', 'bad_id', 'bad_pixiv_id', 'highres', 'long_hair', 'purple_eyes', 'purple_hair', 'school_uniform', 'original', 'thighhighs', 'blush', 'comic', 'touhou', 'translated', 'panties', 'underwear', 'blue_hair', 'brown_eyes', 'serafuku', 'skirt', 'smile', 'blonde_hair', 'blue_eyes', 'hair_ornament', 'hat', 'twintails', 'wings', '1boy', 'boots', 'bow', 'breasts', 'gloves', 'heart', 'holding', 'large_breasts', 'nipples', 'nude', 'animal_ears', 'brown_hair', 'commentary_request', 'japanese_clothes', 'green_eyes', 'one_eye_closed', 'tail', 'absurdres', 'ahoge', 'long_sleeves', 'shirt', 'male_focus', 'red_eyes', 'bare_shoulders', 'jewelry', 'simple_background', 'white_background', 'barefoot', 'flower', 'food', 'green_hair', 'lying', 'multiple_girls', 'sitting', 'black_hair', 'closed_eyes', 'glasses', 'red_hair', 'translation_request', 'weapon', 'full_body', 'multiple_boys', 'standing', 'hair_ribbon', 'open_mouth', 'ribbon', 'yellow_eyes', 'hairband', 'kantai_collection', 'cleavage', '2girls', 'black_legwear', 'ponytail', 'very_long_hair', 'detached_sleeves', 'necktie', 'hair_bow', 'swimsuit', 'dress', 'd', 'navel', 'braid', 'ass', 'pink_hair', 'pantyhose', 'medium_breasts', 'silver_hair'}
+gan_tags		= {'aqua eyes', 'aqua hair', 'black eyes', 'black hair', 
+       'blonde hair', 'blue eyes', 'blue hair', 'blush', 'brown eyes',
+       'brown hair', 'drill hair', 'glasses', 'green eyes', 'green hair',
+       'grey hair', 'hat', 'long hair', 'open mouth', 'orange eyes',
+       'orange hair', 'pink eyes', 'pink hair', 'ponytail', 'purple eyes',
+       'purple hair', 'red eyes', 'red hair', 'ribbon', 'short hair',
+       'silver hair', 'smile', 'twintails', 'white hair', 'yellow eyes'}
+
+
+please_dont 	= {'bangs', 'nipples', 'nude', 'ass'}
+dont_know 		= {'translated', 'd'}
+irrelevant 		= {'bad_id', 'bad_pixiv_id', 'translation_request', 'commentary_request',  'absurdres'}

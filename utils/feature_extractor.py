42935b91e99676452524ac95a13d8aff18fdfb5e
Min Ki Jo <themis0888@gmail.com>
2018-07-28 03:56:33 +0900

    Milestone 1

--- utils/feature_extractor.py
+++ utils/feature_extractor.py
@@ -1,16 +1,16 @@
+"""
+CUDA_VISIBLE_DEVICES=1 python feature_extractor.py \
+--data_path=/home/siit/navi/data/sample/ \
+--list_path=/home/siit/navi/data/sample/meta/ \
+--model_name=vgg_19
+"""
+
 import pdb
-import time
-import random
+import random, time, os, sys
 import numpy as np
 import scipy
 import tensorflow as tf
-import os
-import sys
-#from tensorflow.examples.tutorials.mnist import input_data
-#import alexnet_model
 import tensorflow.contrib.slim.nets as nets
-#import mnist_model_sonic as mnist_model
-#from scipy.io import loadmat
 import data_loader
 import scipy.io as sio
 
@@ -19,8 +19,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', type=str, dest='data_path', default='/home/siit/navi/data/danbooru2017/256px/')
 parser.add_argument('--data_name', type=str, dest='data_name', default='danbooru')
-parser.add_argument('--save_path', type=str, dest='save_path', default=config.data_path + 'meta/')
-parser.add_argument('--model_path', type=str, dest='model_path', default='./vgg_19.ckpt')
+parser.add_argument('--model_name', type=str, dest='model_name', default='vgg_19')
 config, unparsed = parser.parse_known_args() 
 
 
@@ -42,13 +41,8 @@ flags.DEFINE_integer('train_display', 200, '')
 flags.DEFINE_integer('val_display', 1000, '')
 flags.DEFINE_integer('val_iter', 100, '')
 
-#################################################
-########## checkpoint configuration #############
-#################################################
-flags.DEFINE_string('checkpoint_name', '/st1/dhna/sampling/result/checkpoints/imagenet_alexnet_deepset', '')
-flags.DEFINE_integer('save_iter', 50000, '')
-flags.DEFINE_string('ds_save_path', '/st1/dhna/sampling/result/', '')
-flags.DEFINE_string('ds_save_etc', '_d_lr_04_set4_', '')
+
+
 FLAGS = flags.FLAGS
 
 slim = tf.contrib.slim
@@ -57,9 +51,19 @@ vgg = nets.vgg
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=FLAGS.memory_usage)
 sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
 
-# TODO Read imagenet dataset
-awa_train_path = '/st1/dhna/sampling/code/sample_net/awa_whole_list.txt'# NOTE needs to be changed
+awa_train_path = config.data_path + 'meta/path_label_list.txt'
+model_path = '/home/siit/navi/data/models/' + config.model_name + '.ckpt'
 
+# num_file : int 
+# count the number of input image files
+with open(awa_train_path) as f:
+    for num_file, l in enumerate(f):
+        pass
+
+"""
+example) queue_data('/home/siit/navi/data/sample/meta/path_label_list.txt', 
+50, 1, 'val',multi_label=False)
+"""
 trainX, trainY = data_loader.queue_data(
 		awa_train_path, FLAGS.n_classes, FLAGS.batch_size, 'val', multi_label=False)
 
@@ -68,6 +72,7 @@ x = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
 y_ = tf.placeholder(tf.float32, shape=[None, FLAGS.n_classes])
 keep_prob = tf.placeholder(tf.float32)
 
+print("\nLoding the model")
 
 # NOTE Main Network
 with slim.arg_scope(vgg.vgg_arg_scope()):
@@ -79,11 +84,13 @@ var_to_restore = [v for v in all_vars if not v.name.startswith('vgg_19/fc8')]
 
 tf.train.start_queue_runners(sess=sess)
 saver = tf.train.Saver(var_to_restore)
-saver.restore(sess, config.model_path)
+saver.restore(sess, model_path)
+
+print("\nStart Extracting features")
 
 feat = []
 lab = []
-for i in range(30475):
+for i in range(num_file+1):
 	batch_x, batch_y = sess.run([trainX, trainY])
 	_, idx = np.nonzero(batch_y)
 
@@ -91,12 +98,14 @@ for i in range(30475):
 	feat.append(feature[0][0][0])
 	lab.append(idx[0])
 	if i%100 == 0:
-		print (i, " th iteration")
+		print("{} \% done".format(100*i/num_file))
 
 
-if not os.path.exists(config.save_path):
-	os.mkdir(config.save_path)
+save_path = config.data_path + 'meta/'
+if not os.path.exists(save_path):
+	os.mkdir(save_path)
 
-sio.savemat(config.save_path + config.data_name + 'feature_value.mat', {'feature': feat, 'label': lab})
+sio.savemat(save_path + config.model_name + '_feature_prediction.mat', 
+	{'feature': feat, 'label': lab})
 
 print('end')
--- utils/file_list_writer.py
+++ utils/file_list_writer.py
@@ -1,6 +1,6 @@
 """
 python file_list_writer.py \
---data_path=/home/siit/navi/data/sample \
+--data_path=/home/siit/navi/data/sample/ \
 --data_name=sample
 """
 
@@ -17,7 +17,6 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', type=str, dest='data_path', default='/home/siit/navi/data/danbooru2017/256px/')
 parser.add_argument('--data_name', type=str, dest='data_name', default='danbooru')
-parser.add_argument('--save_path', type=str, dest='save_path', default=data_path+'meta/')
 config, unparsed = parser.parse_known_args() 
 
 
@@ -39,10 +38,15 @@ def find_files(paths, extensions, sort=True):
     return files
 
 
+# make the save dir if it is not exists
+save_path = config.data_path + 'meta/'
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
 
+# save the file inside of the meta/ folder
 file_lst = find_files(config.data_path,('.jpg','.png'))
-f = open(config.save_path + config.data_name + 'file_list.txt', 'w')
+f = open(config.data_path + 'meta/' + 'path_label_list.txt', 'w')
 for line in file_lst:
-    f.write(line + '\n' + ' 0')
+    f.write(line + ' 0\n')
 
 f.close()

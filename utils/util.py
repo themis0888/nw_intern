95672aa3433d09f252c6d734970b0da6c41681a6
Min Ki Jo <themis0888@gmail.com>
2018-08-16 12:41:35 +0900

    4d data bug fixer & compressor implemented

--- utils/util.py
+++ utils/util.py
@@ -23,19 +23,33 @@ import scipy.io as sio
 import argparse
 
 def file_list(path, extensions, sort=True, path_label = False):
-    if path_label == True:
-        result = [(os.path.join(dp, f) + ' ' + os.path.join(dp, f).split('/')[-2])
-        for dp, dn, filenames in os.walk(path) 
-        for f in filenames if os.path.splitext(f)[1] in extensions]
-    else:
-        result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) 
-        for f in filenames if os.path.splitext(f)[1] in extensions]
-    if sort:
-        result.sort()
-    return result
+	if path_label == True:
+		result = [(os.path.join(dp, f) + ' ' + os.path.join(dp, f).split('/')[-2])
+		for dp, dn, filenames in os.walk(path) 
+		for f in filenames if os.path.splitext(f)[1] in extensions]
+	else:
+		result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) 
+		for f in filenames if os.path.splitext(f)[1] in extensions]
+	if sort:
+		result.sort()
+	return result
 
 def lost_file_remover(path, exists_list):
-    filenames = file_list(path, ['.jpg'])
-    for filename in filenames:
-        if not os.path.basename(filename) in exists_list:
-            os.remove(filename)
+	filenames = file_list(path, ['.jpg'])
+	for filename in filenames:
+		if not os.path.basename(filename) in exists_list:
+			os.remove(filename)
+
+# ex) '/shared/data/f2c_4dcyc/trainA/np_008083.npy'
+def convert_npy_f2d(path):
+	filenames = file_list(path, ['.npy'])
+	for filename in filenames:
+		npy_file = np.load(filename)
+		[h, w, c] = npy_file.shape
+		for y in range(h):
+			for x in range(w):
+				if npy_file[y, x, -1] != 0:
+					npy_file[y, x, -1] = 255
+					
+		npy_file = np.uint8(npy_file)
+		np.save(filename, npy_file)
